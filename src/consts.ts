d50c3dae2e9274dc41bea4308c09d9afc1f58cba
Adrian Martin <adrianmartinm@outlook.es>
2024-08-10 17:09:30 +0200

    muuuuuchas cosas nuevas

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,4 @@
 
 export const SITE_TITLE = 'Astro Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_GOOGLE_MAP_URL = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3087.0289172987027!2d-4.108138684943202!3d40.90841927930788!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd41406c74decdaf%3A0x1185e147a2a93631!2sLIMMAT!5e0!3m2!1ses-ES!2ses!4v1691672928796!5m2!1ses-ES!2ses'
